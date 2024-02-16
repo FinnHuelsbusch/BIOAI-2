a8bfb0031e8701a95c450ac4bda61ddabb7be3c4
FinnHuelsbusch <f.huelsbusch@outlook.de>
2024-02-16 16:41:29 +0000

    add checks to mutation

--- mutation.cpp
+++ mutation.cpp
@@ -11,6 +11,10 @@ Genome reassignOnePatient(Genome& genome) {
     
     RandomGenerator& rng = RandomGenerator::getInstance();
     int source_nurse = rng.generateRandomInt(0, genome.size() - 1);
+    // check that the source has at least one patient
+    if (genome[source_nurse].size() == 0) {
+        return reassignOnePatient(genome);
+    }
     int destination_nurse = rng.generateRandomInt(0, genome.size() - 1);
     int patient_index = rng.generateRandomInt(0, genome[source_nurse].size() - 1);
     int patient = genome[source_nurse][patient_index];
@@ -18,6 +22,9 @@ Genome reassignOnePatient(Genome& genome) {
     genome[source_nurse].erase(it); 
     //randomly sample insertion point
     int insertion_point = rng.generateRandomInt(0, genome[destination_nurse].size() - 1);
+    if (genome[destination_nurse].size() == 0) {
+        insertion_point = 0;
+    }
     genome[destination_nurse].insert(genome[destination_nurse].begin() + insertion_point, patient);
     return genome;
 }
