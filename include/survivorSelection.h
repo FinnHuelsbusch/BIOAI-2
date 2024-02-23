38df5c474bb710494cf526c654f57e850cc8f5c3
FinnHuelsbusch <f.huelsbusch@outlook.de>
2024-02-23 08:37:14 +0000

    Add roulette wheel survivor selection

--- include/survivorSelection.h
+++ include/survivorSelection.h
@@ -2,3 +2,4 @@
 #include "structures.h"
 
 Population full_replacement(const Population& parents, const Population& children, const function_parameters& parameters);
+Population roulette_wheel_replacement(const Population& parents, const Population& children, const function_parameters& parameters);
--- src/survivorSelection.cpp
+++ src/survivorSelection.cpp
@@ -1,6 +1,18 @@
 #include "structures.h"
 #include "survivorSelection.h"
+#include <parentSelection.h>
 
 Population full_replacement(const Population& parents, const Population& children, const function_parameters& parameters) {
     return children;
+}
+
+Population roulette_wheel_replacement(const Population& parents, const Population& children, const function_parameters& parameters) {
+    const int population_size = parents.size();
+    const int number_of_children = children.size();
+    Population combined_population = parents;
+    combined_population.insert(combined_population.end(), children.begin(), children.end());
+    Population survivors = roulette_wheel_selection(combined_population, parameters);
+    // limit survivors to the population size
+    survivors.resize(population_size);
+    return survivors;
 }
